9d73c8d055551c89a9641c039a44ec4d66f85788
svetultus <svetultus@gmail.com>
2019-07-30 18:47:07 +0300

    homework_1

---
+++ src/components/App/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="description">Миру-мир, студентам - beer.</p>
+    </div>
+  );
+}
+export default App;
